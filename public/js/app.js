adb9b81c78bf3ddcee3a73ee34357b0e85b4e656
GabrielDCelery <gabriel.d.celery@gmail.com>
2016-04-12 13:37:20 +0100

    TASK 4 completed (now the user can add new colour palettes)

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ VARIABLES
 ************************************************************************/	
 
 	$scope.jsonPalette = [];
-	$scope.palette =[];
+	$scope.palette = [];
 
 	$scope.slider = {
 		rgb: [0, 0, 0],
@@ -32,6 +32,10 @@ FUNCTIONS
 		}
 		return array;
 	}
+
+/************************************************************************
+COLOUR PALETTE MODIFIER FUNCTIONS
+************************************************************************/
 
 	function setSliders(rgb, opacity, index){
 		currentlySelected = index;
@@ -57,6 +61,16 @@ FUNCTIONS
 		$scope.palette[currentlySelected].opacity = opa;
 	}
 
+	function duplicateColour(){
+		var newColourScheme = angular.copy($scope.palette[currentlySelected]);
+		$scope.palette.unshift(newColourScheme);
+		currentlySelected = 0;
+	}
+
+/************************************************************************
+DATABASE FUNCTIONS
+************************************************************************/
+
 	function getPalette(num){
 		ApiFactory.getPalette(num, function(response){
 			$scope.jsonData = response;
@@ -69,10 +83,11 @@ FUNCTIONS
 EVENT BINDERS
 ************************************************************************/
 
-	$scope.getPalette = getPalette;
 	$scope.setSliders = setSliders;
 	$scope.modifyColour = modifyColour;
 	$scope.modifyOpacity = modifyOpacity;
+	$scope.duplicateColour = duplicateColour;
+	$scope.getPalette = getPalette;
 
 /************************************************************************
 INITIATING FUNCTION UPON LOADING
--- src/js/controllers/controller_palette.js
+++ src/js/controllers/controller_palette.js
@@ -10,7 +10,7 @@ VARIABLES
 ************************************************************************/	
 
 	$scope.jsonPalette = [];
-	$scope.palette =[];
+	$scope.palette = [];
 
 	$scope.slider = {
 		rgb: [0, 0, 0],
@@ -29,6 +29,10 @@ FUNCTIONS
 		}
 		return array;
 	}
+
+/************************************************************************
+COLOUR PALETTE MODIFIER FUNCTIONS
+************************************************************************/
 
 	function setSliders(rgb, opacity, index){
 		currentlySelected = index;
@@ -54,6 +58,16 @@ FUNCTIONS
 		$scope.palette[currentlySelected].opacity = opa;
 	}
 
+	function duplicateColour(){
+		var newColourScheme = angular.copy($scope.palette[currentlySelected]);
+		$scope.palette.unshift(newColourScheme);
+		currentlySelected = 0;
+	}
+
+/************************************************************************
+DATABASE FUNCTIONS
+************************************************************************/
+
 	function getPalette(num){
 		ApiFactory.getPalette(num, function(response){
 			$scope.jsonData = response;
@@ -66,10 +80,11 @@ FUNCTIONS
 EVENT BINDERS
 ************************************************************************/
 
-	$scope.getPalette = getPalette;
 	$scope.setSliders = setSliders;
 	$scope.modifyColour = modifyColour;
 	$scope.modifyOpacity = modifyOpacity;
+	$scope.duplicateColour = duplicateColour;
+	$scope.getPalette = getPalette;
 
 /************************************************************************
 INITIATING FUNCTION UPON LOADING
